637e19ac84b9313419027d254bfbd1aeb641752a
鱼说还休 <messfairy@gmail.com>
2016-04-06 23:04:26 +0800

    mmm

---
+++ multiline.js
@@ -0,0 +1,60 @@
+'use strict';
+var stripIndent = function (str) {
+  var match = str.match(/^[ \t]*(?=\S)/gm);
+
+  if (!match) {
+    return str;
+  }
+
+  var indent = Math.min.apply(Math, match.map(function (el) {
+    return el.length;
+  }));
+
+  var re = new RegExp('^[ \\t]{' + indent + '}', 'gm');
+
+  return indent > 0 ? str.replace(re, '') : str;
+};
+
+// start matching after: comment start block => ! or @preserve => optional whitespace => newline
+// stop matching before: last newline => optional whitespace => comment end block
+var reCommentContents = /\/\*!?(?:\@preserve)?[ \t]*(?:\r\n|\n)([\s\S]*?)(?:\r\n|\n)[ \t]*\*\//;
+
+var multiline = module.exports = function (fn) {
+  if (typeof fn !== 'function') {
+    throw new TypeError('Expected a function');
+  }
+
+  var match = reCommentContents.exec(fn.toString());
+
+  if (!match) {
+    throw new TypeError('Multiline comment missing.');
+  }
+
+  return match[1];
+};
+
+multiline.stripIndent = function (fn) {
+  return stripIndent(multiline(fn));
+};
+
+var abcf = function(){/*@preserve
+ hahahahahha
+ zhdflj
+ fkldkf
+ */return 'template'};
+var abc = multiline.stripIndent(abcf);
+
+var adcf = function(){/*@preserve
+ hahahahahha
+ zhdflj
+ fkldkf
+ */return 'text'};
+
+var adc = multiline(adcf);
+console.log(abcf());
+console.log(abc);
+console.log(abc.length);
+console.log();
+console.log(adcf());
+console.log(adc);
+console.log(adc.length);
