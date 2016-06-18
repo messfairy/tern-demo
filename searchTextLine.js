d67083b09f0caed29af8be73e6ceab768d9fbfe3
鱼说还休 <messfairy@gmail.com>
2016-06-18 20:42:34 +0800

    mmm

--- searchTextLine.js
+++ searchTextLine.js
@@ -1,12 +1,12 @@
 var fs = require('fs');
 fs.readFile('./main.js', function(err, buffer){
-    var lineArray = searchByLine('exports', buffer.toString());
+    var lineArray = searchLine('require', buffer.toString());
     lineArray.forEach(function(line){
         console.log('line: ' + line.line + ', ch: ' + line.ch + ', text: ' + line.text);
     });
 });
 
-function searchByLine(target, text){
+function searchLine(target, text){
     var textList = text.split('\n');
     var lineArray = [];
     textList.forEach(function(text, line){
