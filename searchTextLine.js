43e0d27ac6dc060c91fa96e2cf0dc62c49e7e614
鱼说还休 <messfairy@gmail.com>
2016-06-18 20:39:18 +0800

    mmm

---
+++ searchTextLine.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+fs.readFile('./main.js', function(err, buffer){
+    var lineArray = searchByLine('exports', buffer.toString());
+    lineArray.forEach(function(line){
+        console.log('line: ' + line.line + ', ch: ' + line.ch + ', text: ' + line.text);
+    });
+});
+
+function searchByLine(target, text){
+    var textList = text.split('\n');
+    var lineArray = [];
+    textList.forEach(function(text, line){
+        var ch = text.search(target);
+        if(ch>-1) lineArray.push({text: text, ch: ch, line: line});
+    });
+    return lineArray;
+}
